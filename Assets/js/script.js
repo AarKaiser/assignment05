6149704e8e042c4a0051b4f6192c310b456d0ec2
Aar Kaiser <studies@mohussein.com>
2021-09-28 22:06:36 -0400

    updating files

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -53,5 +53,41 @@ function startPlanner(){
 
 startPlanner();
 
+let containerEl = $(".container");
+
+let timeArr = ["8AM", "9AM", "10AM", "11AM", "12PM", "1PM"];
+let currentHour = parseInt(moment().format("HH"));
+
+function saveToLocal() {
+  console.log("to be done later");
+  console.log($(this));
+}
+
+for (i = 0; i < timeArr.length; i++) {
+  let newRow = $("<div>").attr("class", "row");
+  let newP = $("<p>").attr("class", "col-2 hour").text(timeArr[i]);
+  let newText = $("<textarea>");
+
+  if (i + 9 < currentHour) {
+    newText.attr("class", "col-8 past");
+  } else if (currentHour == i + 9) {
+    newText.attr("class", "col-8 present");
+  } else {
+    newText.attr("class", "col-8 future");
+  }
+
+  let newBtn = $("<button>")
+    .attr("class", "col-2 saveBtn")
+    .text("Save")
+    .on("click", saveToLocal);
+
+  newRow.append(newP);
+  newRow.append(newText);
+  newRow.append(newBtn);
+
+  containerEl.append(newRow);
+}
+
+
 
 // Submitting incomplete, as I have no idea how to make this work and need extra help from my tutor //
